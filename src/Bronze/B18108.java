19804125790941ec4830ea5b53062e88de82d556
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-07 22:24:22 +0900

    1107

--- src/Bronze/B18108.java
+++ src/Bronze/B18108.java
@@ -1,7 +1,13 @@
 package Bronze;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class B18108 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int bulgi = Integer.parseInt(in.readLine());
+        System.out.println(bulgi - 543);
+
     }
 }
---
+++ src/Bronze/B9498.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B9498 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(in.readLine());
+
+        if (A >= 90 && A <= 100) {
+            System.out.println("A");
+        } else if (A >= 80 && A <= 89) {
+            System.out.println("B");
+        } else if (A >= 70 && A <= 79) {
+            System.out.println("C");
+        } else if (A >= 60 && A <= 69) {
+            System.out.println("D");
+        }
+    }
+}
