ad33b79a9c96ed4b174a5bc233d8e17aaafe8e53
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-10 22:45:50 +0900

    1110

--- src/Silver/S25206.java
+++ src/Silver/S25206.java
@@ -6,7 +6,10 @@ import java.io.InputStreamReader;
 public class S25206 {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String s = in.readLine();
+        for (int i = 0; i < 20; i++) {
+            String[] s = in.readLine().split(" ");
+
+        }
 
     }
 }
