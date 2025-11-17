c92fdf79404576bf755d735711a25802b025b719
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-17 23:16:57 +0900

    1117

--- src/Bronze/B2738.java
+++ src/Bronze/B2738.java
@@ -2,50 +2,45 @@ package Bronze;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class B2738 {
     public static int[][] A;
     public static int[][] B;
     public static int[][] C;
-
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        String[] split = in.readLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int M = Integer.parseInt(split[1]);
 
         A = new int[N][M];
         B = new int[N][M];
         C = new int[N][M];
-
-        // A 입력
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(in.readLine());
+            String[] s = in.readLine().split(" ");
             for (int j = 0; j < M; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+                A[i][j] = Integer.parseInt(s[j]);
             }
         }
 
-        // B 입력
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(in.readLine());
+            String[] s = in.readLine().split(" ");
             for (int j = 0; j < M; j++) {
-                B[i][j] = Integer.parseInt(st.nextToken());
+                B[i][j] = Integer.parseInt(s[j]);
             }
         }
 
-        // C = A + B
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 C[i][j] = A[i][j] + B[i][j];
-                sb.append(C[i][j]).append(" ");
             }
-            sb.append("\n");
         }
 
-        System.out.print(sb.toString());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
